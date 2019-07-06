3a1a9f7d21fa49641707516c248d6f6a0680ec42
Marcus Alvarez <malvarez@mednet.ucla.edu>
2019-07-06 16:43:41 -0700

    fet enrichment

---
+++ R/enrich.R
@@ -0,0 +1,75 @@
+
+
+# genes is a character vector of genes to test enrichment for
+# background genes is a character vector of genes that are used as the background
+# dir_go contains the GO files
+# species is "human" or "mgi"
+# min_genes only tests terms with at least this many genes
+# method_correct corrects the FET p-value using this method. FDR by default
+fet_go <- function(genes, 
+				   bg_genes=NULL, 
+				   dir_go="data/ref/GO/", 
+				   species="human", 
+				   min_genes=30, 
+				   method_correct="fdr"){
+	# GO term annotations
+	go_anno_fn <- "data/ref/go/GO_id.name.namespace.txt"
+	go_anno <- read.table(go_anno_fn, row.names=1, header=TRUE, stringsAsFactors=FALSE, sep="\t", comment="", quote="")
+
+	# Read in annotations
+	anno_fn <- paste0(dir_go, "GO_gene_terms.annotations.", species, ".txt")
+	anno <- read.table(anno_fn, row.names=1, header=TRUE, stringsAsFactors=FALSE, sep="\t", comment="", quote="")
+
+	# Read in GO terms and their genes
+	terms_fn <- paste0(dir_go, "GO_terms.gene_list.", species, ".txt")
+	terms <- read.table(terms_fn, row.names=1, header=TRUE, stringsAsFactors=FALSE, sep="\t", comment="", quote="")
+	
+	# Set genes and background
+	genes <- intersect(genes, rownames(anno))
+	if (length(genes) == 0){
+		stop("No genes given that are found in the GO annotation file")
+	}
+	if (is.null(bg_genes)){
+		bg_genes <- rownames(anno)
+	}
+	bg_genes <- setdiff(bg_genes, genes)
+	if (length(genes) == 0){
+		stop("No background genes found in the GO annotation file")
+	}
+
+	# Loop through annotations
+	enr <- list()
+	for (term in rownames(terms)){
+		term_genes <- terms[term,"Genes"]
+		term_genes <- base::strsplit(x=term_genes, split=";")
+		if (length(term_genes) < min_genes){
+			next
+		}
+		# FET setup
+		#  s1 | s2
+		#  -------
+		#  s3 | s4
+		s1 <- base::intersect(genes, term_genes) # Genes in term
+		s2 <- base::set_diff(genes, term_genes) # Genes NOT in term
+		s3 <- base::intersect(bg_genes, term_genes) # BG in term
+		s4 <- base::set_diff(bg_genes, term_genes) # BG NOT in term
+		m <- matrix(c(length(s1), length(s2), length(s3), length(s4)), 
+					nrow=2, ncol=2, byrow=TRUE)
+		fet <- fisher.test(m)
+		ret <- c(Name=go_anno[term, "Name"], 
+				 NameSpace=go_anno[term, "NameSpace"], 
+				 OR=fet$estimate, 
+				 Null=fet$null.value, 
+				 p=fet$p.value, 
+				 GenesInTerm=length(s1), 
+				 GenesOutTerm=length(s2), 
+				 TermGenes=length(term_genes), 
+				 Genes=s1)
+		enr[[term]]
+	}
+	enr_df <- do.call(base::rbind, enr)
+	enr_df$p_adj <- p.adjust(enr_df$p, method=method_correct)
+	enr_df <- enr_df[order(enr_df$p_adj, decreasing=FALSE),]
+	return(enr_df)
+}
+
---
+++ README.md
@@ -0,0 +1,59 @@
+
+# GO Enrichment
+
+## Downloading GO terms
+
+Use the script:
+
+```bash
+Rscript R/get_go.R
+```
+
+to download the latest gene annotations from GO. They are 
+downloaded to `data/ref/GO`. If you don't want to download them, you can 
+use the ones already present downloaded July 6th 2019.
+
+First, the gene ontologies themselves are downloaded to 
+`data/ref/go/GO_id.name.namespace.txt`. This contains info on the 
+ontology terms. The rownames are the GO ID (e.g. GO:0000001). The 
+first column is the name of the GO term (e.g. mitochondrion inheritance) 
+and the second column is the namespace (one of **biological_process**, 
+**molecular_function** or **cellular component**). 
+
+After downloading the ontologies, the genes are annotated with them. The 
+annotations are downloaded to `data/ref/go/GO_gene_terms.human.txt` and 
+`data/ref/go/GO_gene_terms.mgi.txt`. The row names are the gene IDs, the 
+first column is the UniProtID, the second column is a description of the 
+gene, and the third column contains the GO term IDs associated with gene. 
+If there are multiple GO terms associated with the gene, they are separated 
+by semicolons.
+
+After getting the gene annotations, they are expanded so that we can directly 
+read all the ontologies instead of the GO term ID. These expanded annotations 
+are placed in the files `data/ref/go/GO_gene_terms.annotations.human.txt` and 
+`data/ref/go/GO_gene_terms.annotations.mgi.txt`. The first columns are the 
+same as the annotations: the row names are the gene IDs, the first column 
+is the UniProtID, the second column is a description of the gene, and the 
+third column contains the GO term IDs associated with gene. Then, the 
+fourth, fifth, and sixth column contain the **MolecularFunction**, 
+**BiologicalProcess**, and **CellularComponent** terms associated with the 
+gene. Multiple terms are separated by semicolons.
+
+Then, the reverse is done where for each GO term ID, all the gene names associated 
+with it are listed in the first column. The files are in 
+`data/ref/go/GO_terms.gene_list.human.txt` and 
+`data/ref/go/GO_terms.gene_list.mgi.txt`.
+
+## Functions
+
+With the GO data downloaded above, you can use the functions to test for 
+enrichment based on a gene set.
+
+The function `fet_go` is used to test enrichment of a set of genes using 
+a Fisher's Exact Test (FET). 
+
+```R
+
+go_df <- fet_go(gene_set, bg_genes=NULL, dir_go="data/ref/GO/")
+
+```
